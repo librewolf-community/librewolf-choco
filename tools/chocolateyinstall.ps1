c6f43e17c3f478e000cef52e3f40047901903b44
Malte Jürgens <maltejur@web.de>
2021-04-20 22:33:51 +0200

    Add templates

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64      = 'https://gitlab.com/librewolf-community/browser/windows/uploads/25c11fdf3e0489ccc82e261f493753f6/librewolf-88.0.en-US.win64-setup.exe'
+$url64      = {{file}}
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -10,7 +10,7 @@ $packageArgs = @{
 
   softwareName  = 'LibreWolf'
 
-  checksum64    = 'CE0D822E45D5FAD9C324CE79EB4FB9D2B4F80CF83984EFBBF8A699849DBC5088'
+  checksum64    = {{checksum}}
   checksumType64= 'sha256'
 
   silentArgs   = '/S'
